53d0083439ccbaeae79e8aeba87be5049246f0e6
Chris Pryer <pryerc1@students.rowan.edu>
2016-11-13 19:28:41 -0500

    pushing hackathon project

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+var gulp = require('gulp');
+var less = require('gulp-less');
+var browserSync = require('browser-sync').create();
+var header = require('gulp-header');
+var cleanCSS = require('gulp-clean-css');
+var rename = require("gulp-rename");
+var uglify = require('gulp-uglify');
+
+gulp.task('copy', function() {
+  gulp.src(['node_modules/d3/build/d3.js', 'node_modules/d3/build/d3.min.js'])
+    .pipe(gulp.dest('public/vendor/d3/build'));
+
+  gulp.src(['node_modules/d3-*/build/d3-*.js', 'node_modules/d3-*/build/d3-*.min.js'])
+    .pipe(gulp.dest('public/vendor/'));
+
+  gulp.src(['node_modules/three/build/three.js', 'node_modules/three/build/three.min.js'])
+    .pipe(gulp.dest('public/vendor/three/build'));
+});
+
+gulp.task('default', ['copy']);
---
+++ magnitude.py
@@ -0,0 +1,41 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import csv
+import json
+
+with open('meteorite-landings.csv', 'rb') as file:
+    reader = csv.reader(file)
+    X = list(reader)
+
+def hasNumbers(inputString):
+    return any(char.isdigit() for char in inputString)
+
+def feature_scale(nparray):
+    return (nparray - np.amin(nparray))/(np.amax(nparray) - np.amin(nparray))
+
+i = 0
+while i < len(X):
+    try:
+        float(X[i][4])
+        float(X[i][7])
+        float(X[i][8])
+        i = i + 1
+    except ValueError:
+        del X[i]
+
+X = np.asarray(X)
+
+mass = X[1:,4].astype(np.float)
+new_mass = np.log(np.log(np.sort(mass)+1)+1)
+new_mass = (new_mass - np.amin(new_mass))/(np.amax(new_mass) - np.amin(new_mass))
+
+indices = feature_scale(np.arange(mass.shape[0]).astype(float))
+
+plt.plot(new_mass, color="red")
+plt.plot(indices, color="blue")
+plt.plot(feature_scale(np.log(np.sort(mass).astype(float) + 1)), color="purple")
+plt.plot(feature_scale(np.sort(mass).astype(float)), color='green')
+
+plt.ylabel('Mass')
+plt.xlabel('Index')
+plt.show()
